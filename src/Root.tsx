27f0d1cc9efbbd0cb3e9ba1f7cf026133c064821
sebasor09 <davisebasjauregui96@gmail.com>
2026-06-25 21:12:46 -0500

    cripto finanzas

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,22 +3,17 @@ import { MyVideo } from "./videos/MyVideo";
 import scriptData from "../youtube/finanzas/cripto-finanzas/script.json";
 
 const FPS = 30;
-const totalFrames = scriptData.scenes.reduce(
-  (acc, scene) => acc + scene.duration * FPS,
-  0
-);
+const totalFrames = scriptData.scenes.reduce((acc, s) => acc + s.duration * FPS, 0);
 
 export const RemotionRoot: React.FC = () => {
   return (
-    <>
-      <Composition
-        id="MyVideo"
-        component={MyVideo}
-        durationInFrames={totalFrames}
-        fps={FPS}
-        width={1920}
-        height={1080}
-      />
-    </>
+    <Composition
+      id="MyVideo"
+      component={MyVideo}
+      durationInFrames={totalFrames}
+      fps={FPS}
+      width={1920}
+      height={1080}
+    />
   );
 };
